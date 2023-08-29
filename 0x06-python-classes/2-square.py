808c7624a9dec218e84f83cbd0cca31d6a2cb223
Elijah <akinnodielijah@gmail.com>
2023-08-29 19:02:20 +0000

    updating all works

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this is a square with a private instance"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with optional size.
+    """
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
